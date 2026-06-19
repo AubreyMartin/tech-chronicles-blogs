e84f5143da8ba955b1fe95c6e8f49417cd1f86b8
Aubrey Anup Martin <18359906+AubreyMartin@users.noreply.github.com>
2026-06-19 19:20:42 +0530

    Add category and author filters to the all blogs page.

--- src/user/AllBlogsPage.jsx
+++ src/user/AllBlogsPage.jsx
@@ -3,6 +3,20 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 function AllBlogsPage() {
   const [blogs, setBlogs] = useState([]);
+  const [category, setCategory] = useState("");
+  const [author, setAuthor] = useState("");
+
+  const categories = [...new Set(blogs.map((blog) => blog.category))];
+
+  const authors = [...new Set(blogs.map((blog) => blog.author))];
+
+  const filteredBlogs = blogs.filter((blog) => {
+    const categoryMatch = category === "" || blog.category === category;
+
+    const authorMatch = author === "" || blog.author === author;
+
+    return categoryMatch && authorMatch;
+  });
 
   useEffect(() => {
     axios.get("http://localhost:3000/blogs").then((response) => {
@@ -13,8 +27,27 @@ function AllBlogsPage() {
   return (
     <div>
       <h2>All Blogs</h2>
+      <select value={category} onChange={(e) => setCategory(e.target.value)}>
+        <option value="">All Categories</option>
+
+        {categories.map((cat) => (
+          <option key={cat} value={cat}>
+            {cat}
+          </option>
+        ))}
+      </select>
+
+      <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+        <option value="">All Authors</option>
+
+        {authors.map((auth) => (
+          <option key={auth} value={auth}>
+            {auth}
+          </option>
+        ))}
+      </select>
       <ul>
-        {blogs.map((blog) => (
+        {filteredBlogs.map((blog) => (
           <li key={blog.id}>
             <Link to={`/blog/${blog.id}`}>{blog.title}</Link>
           </li>
